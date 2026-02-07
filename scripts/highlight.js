2ba66cf524603db0e5b963c0fefe596e19042afd
EugouIce <160041733+EugouIce@users.noreply.github.com>
2026-02-08 00:06:29 +0800

    Delete scripts directory

--- scripts/highlight.js
+++
@@ -1,5 +0,0 @@
-function highlightMenu(elem) {
-    let links = document.querySelectorAll(".sidebar a");
-    links.forEach(link => link.classList.remove("active"));
-    if (elem) elem.classList.add("active");
-}
--- scripts/section.js
+++
@@ -1,6 +0,0 @@
-function showSection(id, elem) {
-    let sections = ["dashboard", "profile", "subjects", "grades"];
-    sections.forEach(sec => document.getElementById(sec).classList.add("hidden"));
-    document.getElementById(id).classList.remove("hidden");
-    highlightMenu(elem);
-}
